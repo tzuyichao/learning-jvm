0ee7931f9be0c84084ac7acb31d383905c335c01
TERENCE.CHAO <terence.chao@deltaww.com>
2020-09-03 16:42:34 +0800

    parse test

---
+++ src/main/java/compile/ParseTest.java
@@ -0,0 +1,24 @@
+package compile;
+
+import com.sun.tools.javac.parser.Scanner;
+import com.sun.tools.javac.parser.ScannerFactory;
+import com.sun.tools.javac.parser.Tokens;
+import com.sun.tools.javac.util.Context;
+
+public class ParseTest {
+    public static void main(String[] args) {
+        ScannerFactory scannerFactory = ScannerFactory.instance(new Context());
+        Scanner scanner = scannerFactory.newScanner("int k = i + j;", false);
+
+        scanner.nextToken();
+        Tokens.Token token = scanner.token();
+        while(token.kind != Tokens.TokenKind.EOF) {
+            if(token.kind == Tokens.TokenKind.IDENTIFIER) {
+                System.out.print(token.name() + " - ");
+            }
+            System.out.println(token.kind);
+            scanner.nextToken();
+            token = scanner.token();;
+        }
+    }
+}
