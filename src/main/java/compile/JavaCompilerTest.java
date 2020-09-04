af1adb5ee6d7cf388f27f6709318972e27002068
TERENCE.CHAO <terence.chao@deltaww.com>
2020-09-04 11:22:32 +0800

    compiler test

--- src/main/java/compile/JavaCompilerTest.java
+++ src/main/java/compile/JavaCompilerTest.java
@@ -1,14 +1,11 @@
 package compile;
 
 import com.sun.source.tree.Tree;
-import com.sun.tools.javac.api.JavacTrees;
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.main.Option;
 import com.sun.tools.javac.tree.JCTree;
-import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.Names;
 import com.sun.tools.javac.util.Options;
 
 public class JavaCompilerTest {
@@ -21,9 +18,9 @@ public class JavaCompilerTest {
         compiler.keepComments = true;
 
         JCTree.JCCompilationUnit compilationUnit = compiler.parse("src/main/java/client/User.java");
-        JavacTrees javacTrees = JavacTrees.instance(context);
-        TreeMaker treeMaker = TreeMaker.instance(context);
-        Names names = Names.instance(context);
+//        JavacTrees javacTrees = JavacTrees.instance(context);
+//        TreeMaker treeMaker = TreeMaker.instance(context);
+//        Names names = Names.instance(context);
 
         compilationUnit.defs.stream()
                 .forEach(jcTree -> {
@@ -31,6 +28,8 @@ public class JavaCompilerTest {
                     System.out.println(jcTree.getKind());
                     listVariable(jcTree);
                 });
+
+
     }
 
     private static void listVariable(JCTree tree) {
