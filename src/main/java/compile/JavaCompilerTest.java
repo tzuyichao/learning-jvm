758bd9ca2437a6da2aab7deaa8b5d70d052d88e8
TERENCE.CHAO <terence.chao@deltaww.com>
2020-09-04 10:59:09 +0800

    compiler test

--- src/main/java/compile/JavaCompilerTest.java
+++ src/main/java/compile/JavaCompilerTest.java
@@ -1,10 +1,12 @@
 package compile;
 
+import com.sun.source.tree.Tree;
 import com.sun.tools.javac.api.JavacTrees;
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.main.Option;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
+import com.sun.tools.javac.tree.TreeTranslator;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.Names;
 import com.sun.tools.javac.util.Options;
@@ -23,9 +25,25 @@ public class JavaCompilerTest {
         TreeMaker treeMaker = TreeMaker.instance(context);
         Names names = Names.instance(context);
 
-        JCTree.JCExpression client = treeMaker.Ident(names.fromString("client"));
-        JCTree.JCExpression user = treeMaker.Select(client, names.fromString("User"));
-        System.out.println(user.toString());
+        compilationUnit.defs.stream()
+                .forEach(jcTree -> {
+                    System.out.println(jcTree.toString());
+                    System.out.println(jcTree.getKind());
+                    listVariable(jcTree);
+                });
+    }
 
+    private static void listVariable(JCTree tree) {
+        tree.accept(new TreeTranslator() {
+            @Override
+            public void visitClassDef(JCTree.JCClassDecl classDecl) {
+                classDecl.defs.stream()
+                        .filter(it -> it.getKind().equals(Tree.Kind.VARIABLE))
+                        .map(it -> (JCTree.JCVariableDecl) it)
+                        .forEach(it -> {
+                            System.out.println(it.getName() + ": " + it.getKind() + ": " + it.getType());
+                        });
+            }
+        });
     }
 }
